36d52df8e17bcadb7ab8c971fb73a50b6ba82805
O1iaiaoiiiai <loganh357@outlook.com>
2025-11-16 23:46:59 +0000

    OIIAIAOIIIAI

--- main.py
+++ main.py
@@ -1,2 +1,45 @@
-print("Sorry, still in construction. Thank you.")
-print("- Logan hampton, 3:03, 11/16/25")
+import smtplib
+from email.mime.text import MIMEText
+
+def mailgmail(address, password, subject, body, to_address):
+    # Gmail SMTP settings
+    GMAIL_ADDRESS = address
+    GMAIL_PASSWORD = password  # Use App Password, not regular password
+
+    msg = MIMEText(body)
+    msg['Subject'] = subject
+    msg['From'] = address
+    msg['To'] = to_address
+
+    # Connect to Gmail's SMTP server
+    with smtplib.SMTP_SSL('smtp.gmail.com', 465) as server:
+        server.login(GMAIL_ADDRESS, GMAIL_PASSWORD)
+        print("Preview will be listed below.")
+        print(f"Subject: {subject}")
+        print(f"From: {address}")
+        print(f"To: {to_address}")
+        print()
+        print(f"{body}")
+        if input("Send? y/n") == "Y" or "y":
+            server.send_message(msg)
+        else:
+            print("ABORT")
+print("Hey! No need for argument for they are going to be asked ")
+print("But before we do begin, we have some requirements.")
+print("We require you have an app password from google saved to a TXT file.")
+app_password = input("Do you have one set up? If so, type it here.\nIf you do bot have an app password, please set one up and save it to a TXT file. Please name it OiiaTerminal. We will also require you setup 2FA on your gmail account.\nAPP PASSWORD >> ")
+print(f"App password set to {app_password}.\nIf this is incorrect, please restart the terminal and try again.")
+print("We also require a name. Please type one below.")
+name = input("NAME >> ")
+print("We also require an email.")
+emailaddr = input("EMAIL ADDRESS (must be a @gmail.com email) >> ")
+input("We will not show anymore until you confirm by hitting ENTER that your email address has SMTP on, you can look into this by setting it up through the gmail.com page. (via https://support.google.com/mail/answer/7126229)")
+print("We have finished setting up for you. Enjoy your terminal, new or old user!")
+def checkprompt(prompt):
+    if prompt == "gmailsend":
+        body = input("Send (body, required) >>")
+        subj = input("Subject (Required) >>")
+        to = input("To (Required) >>")
+        mailgmail(emailaddr, app_password, subj, body, to)
+while True:
+    prompt = input(f"{name},{emailaddr}*Oiia>")
